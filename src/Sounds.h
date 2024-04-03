bb73f27510998b90e2d0426b25d5b3f4b16d44b6
Toxa <untoxa@mail.ru>
2024-04-03 15:23:10 +0300

    Play WAV file on the game boy targets on the ZGB splash screen

--- src/Sounds.h
+++ src/Sounds.h
@@ -4,19 +4,23 @@
 #include "Sound.h"
 
 BANKREF_EXTERN(FX_JUMP)
-extern void __mute_mask_FX_JUMP;
 extern const UINT8 FX_JUMP[];
+extern void __mute_mask_FX_JUMP;
 
 BANKREF_EXTERN(FX_PICKUP)
-extern void __mute_mask_FX_PICKUP;
 extern const UINT8 FX_PICKUP[];
+extern void __mute_mask_FX_PICKUP;
 
 BANKREF_EXTERN(FX_HIT)
-extern void __mute_mask_FX_HIT;
 extern const UINT8 FX_HIT[];
+extern void __mute_mask_FX_HIT;
 
 BANKREF_EXTERN(FX_HIT_WALL)
-extern void __mute_mask_FX_HIT_WALL;
 extern const UINT8 FX_HIT_WALL[];
+extern void __mute_mask_FX_HIT_WALL;
+
+BANKREF_EXTERN(FX_SHEEP)
+extern const uint8_t FX_SHEEP[];
+extern void __mute_mask_FX_SHEEP;
 
 #endif
--- src/StateSplashZGB.c
+++ src/StateSplashZGB.c
@@ -1,10 +1,13 @@
 #include "Banks/SetAutoBank.h"
+
 #include "main.h"
 #include "Scroll.h"
 #include "Keys.h"
 #include "ZGBMain.h"
 #include "SGB.h"
 #include "Palette.h"
+#include "Sounds.h"
+#include "Music.h"
 
 IMPORT_MAP(splash_zgb);
 IMPORT_MAP(border);
@@ -12,6 +15,7 @@ IMPORT_MAP(border);
 static UINT8 n_frames;
 
 void START(void) {
+	INIT_SOUND();
 	LOAD_SGB_BORDER(border);
 	SetBorderColor(RGB(0, 0, 0));
 	n_frames = 0;
@@ -21,7 +25,13 @@ void START(void) {
 void UPDATE(void) {
 	n_frames ++;
 
-	if(n_frames == 120 || KEY_TICKED(J_START | J_A | J_B)) {
+#ifdef NINTENDO
+	if(n_frames == 30) {
+		ExecuteSFX(BANK(FX_SHEEP), FX_SHEEP, SFX_MUTE_MASK(FX_SHEEP), SFX_PRIORITY_NORMAL);
+	}
+#endif
+
+	if(n_frames == 240 || KEY_TICKED(J_START | J_A | J_B)) {
 		SetState(StateSplash);
 	}
 }
